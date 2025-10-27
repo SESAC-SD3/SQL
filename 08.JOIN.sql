224d9f216119827823fdb16cae7e2d36d7910b72
5chang2 <och8808@gmail.com>
2025-10-27 14:23:47 +0900

    update

--- 08.JOIN.sql
+++ 08.JOIN.sql
@@ -1,7 +1,7 @@
 USE WORLD;
 
-SELECT * FROM city WHERE CountryCode = 'KOR';
-SELECT * FROM country WHERE Code = 'KOR';
+SELECT * FROM city;
+SELECT * FROM country;
 
 
 SELECT
@@ -48,10 +48,11 @@ ON co.Code = ci.CountryCode
 ORDER BY co.Population;
 
 
+SELECT * FROM city;
 SELECT
 	c1.Name, c2.Name
 FROM city c1 INNER JOIN city c2
-ON c1.ID = c2.ID
+ON c1.CountryCode = c2.CountryCode AND c1.ID < c2.ID
 WHERE c1.CountryCode = 'KOR';
 
 
@@ -62,20 +63,58 @@ FROM country co INNER JOIN city ci ON co.Code = ci.CountryCode
 				INNER JOIN countrylanguage cl ON ci.CountryCode = cl.CountryCode
 ;
 
+USE sakila;
 -- 영화(`film`)와 언어(`language`) 테이블을 조인하여 다음을 조회하세요:
 -- - 영화 제목 (`film.title`), 언어 이름 (`language.name`)
+SELECT
+	film.title, language.name
+FROM film INNER JOIN language 
+ON film.language_id = language.language_id;
 
 -- 영화와 카테고리를 조인하여 다음을 조회하세요:
 -- - 영화 제목, 카테고리 이름, 대여료
+SELECT
+	film.title, category.name, film.rental_rate
+FROM film INNER JOIN film_category 
+ON film.film_id = film_category.film_id
+INNER JOIN category
+ON film_category.category_id = category.category_id;
+
 
 -- 모든 고객(`customer`)과 그들의 대여 내역(`rental`)을 조회하세요.
 -- - 고객 이름 (first_name, last_name), 대여 ID (rental_id), 대여 날짜 (rental_date)
+SELECT
+	customer.first_name, customer.last_name,
+    rental.rental_id, rental.rental_date
+FROM customer LEFT JOIN rental
+ON customer.customer_id = rental.customer_id;
+
+
 
 -- 고객별 대여 횟수를 조회하세요. (대여 횟수 0인 고객도 포함)
 -- - 고객 이름, 대여 횟수, 대여 횟수가 많은 순서
+SELECT
+	customer.customer_id,
+    customer.last_name,
+    COUNT(*)
+FROM customer LEFT JOIN rental
+ON customer.customer_id = rental.customer_id
+GROUP BY customer.customer_id
+ORDER BY COUNT(*);
+
 
 -- 같은 상영 시간(`length`)을 가진 영화 쌍을 찾으세요.
 -- - 영화1 제목, 영화2 제목, 상영 시간, 상위 10개
+
+SELECT
+	f1.title, f2.title, f1.length
+FROM film f1 INNER JOIN film f2
+ON f1.length = f2.length
+ORDER BY f1.length DESC
+LIMIT 10;
+
+
+
 
 
 
